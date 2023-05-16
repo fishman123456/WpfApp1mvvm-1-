75c1094c67a76773470b19edee2ad57fcd8df08e
fishman123456 <den9152@yandex.ru>
2023-05-16 22:04:58 +0700

    добавил комментарии

--- WpfApp1mvvm/WpfApp1mvvm/Model_Dog.cs
+++ WpfApp1mvvm/WpfApp1mvvm/Model_Dog.cs
@@ -17,39 +17,48 @@ namespace WpfApp1mvvm
 {
     public class Model_Dog : INotifyPropertyChanged
     {
+        // поля собаки, имя, пояснение, фотка
         public string dogName;
         public string dogDescription;
         public string imgPath;
 
+        // get set имени
         public string DogName
         {
             get { return dogName; }
             set
             {
                 dogName = value;
+                // свойство передающее изменение в поле
                 OnPropertyChanged("DogName");
             }
         }
+        // get set прояснения
         public string DogDescription
         {
             get { return dogDescription; }
             set
             {
                 dogDescription = value;
+                // свойство передающее изменение в поле
                 OnPropertyChanged("DogDescription");
             }
         }
+        // get set фотки
         public string ImgPath
         {
             get { return imgPath; }
             set
             {
                 imgPath = value;
+                // свойство передающее изменение в поле
                 OnPropertyChanged("ImgPath");
             }
         }
-
+        // событие подписанное на изменение обязательное
         public event PropertyChangedEventHandler PropertyChanged;
+        
+        // изменение  
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             if (PropertyChanged != null)
